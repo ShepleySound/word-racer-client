ec978989b87adeab0c793cf5c1706d365933089c
Robert Shepley <shepleysound@gmail.com>
2022-10-06 17:53:43 -0700

    Further proof of life with multiple clients.

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 'use strict';
 
-require('dotenv').config()
+const randomNickname = 'Random Nickname12345'
+
+require('dotenv').config();
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const io = require('socket.io-client');
 const socket = io(process.env.SERVER_URL || 'http://localhost:3001');
 
+let nickname = null;
+
+// rl.once('line', (input) => {
+//   return input || randomNickname;
+// });
+
+
 console.log('Connecting...')
 socket.on('connect', () => {
-  console.log('[INFO]: Welcome to the game')
+  // nickname = process.argv[2];
+  // console.log(getName());
+  console.log('[INFO]: Welcome to the game');
+  console.log('Enter your name: ')
+  rl.once('line', (input) => {
+    const name = input || randomNickname;
+    console.log(`Hello, ${name}`);
+    socket.emit('join-game', name);
+  });
+});
+
+socket.on('joined-event', (name) => {
+  console.log(`${name} joined the server!`);
 });
